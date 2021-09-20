af67fe6ddfb9f4d235af1a74a16cc2a264caab3d
ngdat1711 <dtnguyena5@gmail.com>
2021-09-20 09:42:08 +0700

    Bài 4 Lab2

---
+++ src/vn/edu/giadinh/tuan03/ChuongTrinhLab2Bai4.java
@@ -0,0 +1,41 @@
+package vn.edu.giadinh.tuan03;
+
+import java.until.Scanner;
+
+public class ChuongTrinhLab2Bai4 {
+
+    public static void main(String[] args) {
+        System.out.println("======Menu======");
+        System.out.println(">>1.Giai Phuong Trinh Bac I ");
+        System.out.println(">>2.Giai Phuong Trinh Bac II ");
+        System.out.println(">>3.Tinh Tien Dien ");
+        System.out.println("======Menu======");
+        int suLuaChon;
+        System.out.println("Lua Chon Chuc Nang: ");
+        Scanner bienNhap;
+        bienNhap  = new Scanner(System.in);
+        suLuaChon = bienNhap.nextInt();
+
+
+        switch(suLuaChon){
+            case 1: giaiPTBacI/;break;
+            case 2: giaiPTBacII; break;
+            case 3: tinhTienDien;break;
+            default: System.out.println("Chon Chuc Nang Khac ");
+
+        }
+    }
+
+    static void giaiPTBacI(){
+        System.out.println("Trien Khai code GPTB1....");
+    }
+
+    static void giaiPTBacII(){
+        System.out.println("Trien Khai code GPTBII.... ");
+    }
+
+    static void tinhTienDien(){
+        System.out.println("Trien Khai code Tinh Tien Dien");
+    }
+}
+
