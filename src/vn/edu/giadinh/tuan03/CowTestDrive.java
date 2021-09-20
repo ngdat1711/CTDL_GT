58ccb00e5f29f70748d5ee8fe942e33bcd0e1fd1
ngdat1711 <dtnguyena5@gmail.com>
2021-09-20 11:43:08 +0700

    them class Cow +test drive

---
+++ src/vn/edu/giadinh/tuan03/CowTestDrive.java
@@ -0,0 +1,16 @@
+package vn.edu.giadinh.tuan03;
+
+public class CowTestDrive {
+    public static void main(String[] args) {
+        Cow cow;
+        //coW =  new Cow();
+        cow = new Cow(15);
+        System.out.println("weigt" +cow.weight);
+        
+        cow.weight = 15;
+
+    
+
+    }
+    
+}
