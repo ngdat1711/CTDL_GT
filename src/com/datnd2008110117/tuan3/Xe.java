a9591bb0a118877a01c6f1313ee4e10ac245c6d6
ngdat1711 <dtnguyena5@gmail.com>
2021-09-21 20:29:12 +0700

    Cập Nhật Xe + TestDrive

--- src/com/datnd2008110117/tuan3/Xe.java
+++ src/com/datnd2008110117/tuan3/Xe.java
@@ -5,7 +5,17 @@ public class Xe {
     String tenChuXe;
     String hangSanXuat;
     String dong;
-    String soGiayPhepLaiXe;
-    
+    double soGiayPhepLaiXe;
+    String dungTichXang;
+
+    void inThongTinXeHoi(){
+
+        System.out.println("Tên Chủ Xe: " + tenChuXe);
+        System.out.println("Hãng Sản Xuất Xe: " + hangSanXuat);
+        System.out.println("Dòng Xe: " + dong);
+        System.out.println("Số In Trên Giấy Phép Lái Xe: " + soGiayPhepLaiXe);
+        System.out.println("Dung Tích Bình Xăng Của Xe: " + dungTichXang);
+    }
+
     
 }
---
+++ src/com/datnd2008110117/tuan3/XeTestDrive.java
@@ -0,0 +1,17 @@
+package com.datnd2008110117.tuan3;
+
+public class XeTestDrive {
+    public static void main(String[] args) {
+        Xe xe;
+        xe = new Xe();
+        xe.tenChuXe = "Nguyễn Văn Tèo Em";
+        xe.hangSanXuat = "Toyota";
+        xe.dong = "Vios 2017";
+        xe.soGiayPhepLaiXe = 123456789;
+        xe.dungTichXang = "42 lít ";
+        
+        xe.inThongTinXeHoi();
+
+    }
+    
+}
