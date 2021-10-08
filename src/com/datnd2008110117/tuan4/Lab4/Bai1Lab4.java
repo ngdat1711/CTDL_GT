dddaaac20136f4eed2c4df367f6f39945ef9d1cb
ngdat1711 <dtnguyena5@gmail.com>
2021-10-08 19:32:37 +0700

    bài tập

---
+++ src/com/datnd2008110117/tuan4/Lab4/Bai1Lab4.java
@@ -0,0 +1,22 @@
+package com.datnd2008110117.tuan4.Lab4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Bai1Lab4 {
+    public static void main(String[] args) {
+        double a;
+        String b;
+        double ;
+
+        Scanner tongSo = new Scanner(System.in);
+        ArrayList<Double>list = new ArrayList<>();
+        while(true){
+            
+        }
+
+   
+
+    }   
+    
+}
